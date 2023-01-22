2f32f12043cf0f17cc2988dba7d26005080187dd
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-22 11:31:46 +0000

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
-import React ,{useRef,useState,useEffect} from 'react'
+import React ,{forwardRef ,useRef,useState,useEffect} from 'react'
 import '../styles/App.css';
 import InputField from './InputField.js';
+
 const App = () => {
- 
-//code here 
+  const [values, setValues] = useState('');
+  const [textAreaValue, setTextAreaValaue] = useState('');
+  const newRef = useRef(null);
 
+  const settingValue = () => {
+    setTextAreaValaue(values);
+    setValues('');
+  }
 
+  const focusInput = () => {
+    newRef.current.focus();
+  }
+
+  const handleTextArea = () => {
+
+  }
  
   return (
-    <div>
-    <InputField  ref={newRef}  type="text"/><br/>
+    <>
+    <InputField  ref={newRef} type='text'value={values} setValue={setValues}/><br/>
     <button id="settingValueButton" onClick={settingValue}>Set Value</button>
     <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
+     <textarea id="textarea" value={textAreaValue} onChange={handleTextArea}></textarea>
 
-    </div>
+    </>
   );
 }
 
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,13 +1,19 @@
-import React ,{useRef,useState,useEffect} from 'react';
+import React ,{useRef,useState,useEffect, forwardRef} from 'react';
 import '../styles/App.css';
 
 //complete the following using forward refs concept 
 
-const InputField=()=>{
-    return(
+const InputField = React.forwardRef((props, ref) => {
+  const {values, setValue} = props;
+  const updateInput = (event) => {
+    setValue(event.target.value);
+  }
+
+  return(
     <div>
-      <input id="input" type="text"  ref={ref}/>
+      <input id="input" value={values} ref={ref} type="text" onChange={updateInput} />
     </div>
-    )
-    }
+  )
+})
+
 export default InputField;
